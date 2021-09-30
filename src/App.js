f408b5c8d4fe458a51d107616ce80a5df235c292
Joy Taylor <jcat7414@gmail.com>
2021-09-30 15:22:27 +1000

    Create Home and Memory Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import HomePage from "./pages/HomePage";
+import MemoryPage from "./pages/MemoryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-         This will become the SweetShot Web App
-        </p>
-        
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+
+        <Switch>
+          <Route path="/memory">
+            <MemoryPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/MemoryPage.jsx
@@ -0,0 +1,7 @@
+import React  from "react";
+
+function MemoryPage() {
+    return <h1>This is the SweetShot Memory Page.</h1>;
+}
+
+export default MemoryPage;
